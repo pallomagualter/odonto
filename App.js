c95ca5373edc4cdf292522ba07665aed949aa13c
Palloma Gualter <pallomagualter@gmail.com>
2020-12-14 10:01:50 -0300

    app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Odonto TESTE</Text>
+      <Text style={styles.text}>Odonto</Text>
       <StatusBar style="auto" />
     </View>
   );
